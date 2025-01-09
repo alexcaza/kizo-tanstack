409622790732e8cb412f403baa7ed7582115dac4
Alex Caza <alex@alexcaza.com>
2025-01-09 16:13:41 -0500

    Bump drizzle version

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,6 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-import Database from 'better-sqlite3';
-import * as schema from './schema';
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
 
-const sqlite = new Database(process.env.DB_PATH);
-export const db = drizzle(sqlite, { schema });
+const client = createClient({ url: process.env.DB_FILE_NAME! });
+const db = drizzle({ client });
