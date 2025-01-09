7d91be568e206a4417a5ff86fea8132255689863
Alex Caza <alex@alexcaza.com>
2025-01-09 16:47:51 -0500

    Use path imports with vite

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,17 @@
 import { defineConfig } from "@tanstack/start/config";
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-export default defineConfig({});
+// Require since we're using ES modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+    vite: {
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'app'),
+                '@/db': path.resolve(__dirname, 'db')
+            }
+        }
+    }
+});
